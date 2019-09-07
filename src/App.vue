e1d3792a37f84e63348352aa00fb106307d74fac
Henrik <skinnybeans@gmail.com>
2019-09-07 18:25:52 +1000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-row class="mt-3">
+        <b-col>
+          <b-button variant="outline-primary" @click="generate_deck">Generate Decks</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col class="col-md-6 col-12" v-for="(deck, index) in decks" :key="deck.character">
+          <app-deck :deck=deck :deck_number="index + 1"></app-deck>
+        </b-col>
+      </b-row>
+    </b-container>
+    <hr>
+    <b-container>
+      <b-row>
+        <b-col>
+          <h2 class="text-primary">Settings</h2>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          Settings go here
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import appDeck from './components/Deck.vue';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      character: ['Ariaspes', 'Tegu', 'Miselda', 'Zevrane', 'Jaleesa', 'Raganhar', 'Thenoch', 'Virgiliu', 'Wachiwi'],
+      linage: ['The Animist', 'The Demonologist', 'The Bloodlord', 'The Necromancer', 'The Shapeshifter', 'The Druid'],
+      domain: ['Of the Forgotton Temple', 'Of the Outcast Sanctuary', 'Of the Haunted Forest', 'Of the Screaming Coast', 'Of the Bloodsoaked Fjord', 'Of the Lunatic Asylum', 'Of the Royal Palace', 'Of the Witch Mountain'],
+      decks: [],
+    };
+  },
+  methods: {
+    generate_deck() {
+      this.decks = [];
+
+      // copy avaiable deck options
+      const remainingCharacter = this.character.slice();
+      const remainingLinage = this.linage.slice();
+      const remainingDomain = this.domain.slice();
+
+      while (
+        remainingCharacter.length > 0
+        && remainingLinage.length > 0
+        && remainingDomain.length > 0
+      ) {
+        const characterIndex = Math.floor(Math.random() * remainingCharacter.length);
+        const linageIndex = Math.floor(Math.random() * remainingLinage.length);
+        const domainIndex = Math.floor(Math.random() * remainingDomain.length);
+        this.decks.push({
+          character: remainingCharacter[characterIndex],
+          linage: remainingLinage[linageIndex],
+          domain: remainingDomain[domainIndex],
+        });
+        remainingCharacter.splice(characterIndex, 1);
+        remainingLinage.splice(linageIndex, 1);
+        remainingDomain.splice(domainIndex, 1);
+      }
+    },
+  },
   components: {
-    HelloWorld,
+    appDeck,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Deck.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-card
+    header-bg-variant="primary"
+    header-text-variant="white"
+    border-variant="primary"
+    header-tag="header"
+    class="my-3"
+    >
+    <template v-slot:header>
+      <h6 class="mb-0">Deck {{ deck_number }}</h6>
+    </template>
+    <b-card-text>{{ deck.character }}</b-card-text>
+    <b-card-text>{{ deck.linage }}</b-card-text>
+    <b-card-text>{{ deck.domain }}</b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: ['deck', 'deck_number'],
+};
+</script>
