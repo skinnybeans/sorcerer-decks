42a000309529eb5107a0b97275a3a97fd7df5083
Henrik <skinnybeans@gmail.com>
2019-09-08 20:10:24 +1000

    fixed some small device display issues

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -1,13 +1,13 @@
 <template>
   <b-card
-    header-bg-variant="dark"
-    header-text-variant="white"
+    header-bg-variant="light"
+    header-text-variant="black"
     border-variant="dark"
     header-tag="header"
     class="my-3"
     >
     <template v-slot:header>
-      <h6 class="mb-0">Deck {{ deck_number }}</h6>
+      <h6 class="mb-0 font-weight-bold">Deck {{ deck_number }}</h6>
     </template>
     <b-card-text>{{ deck.character }}</b-card-text>
     <b-card-text>{{ deck.linage }}</b-card-text>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,14 @@
 <template>
     <b-navbar variant="dark" type="dark" fixed="top" :sticky="true">
-      <b-navbar-brand class="d-flex align-items-center">
-        <img style="width: 300px" src="../assets/sorcerer.png" alt="">
-        <h1 class="d-inline-block mb-0">
-          Decks
-        </h1>
+      <b-navbar-brand class="d-flex align-items-center justify-content-center flex-wrap">
+        <div>
+          <img style="width: 300px" src="../assets/sorcerer.png" alt="">
+        </div>
+        <div>
+          <h1 class="d-inline-block mb-0 col-12 col-md-6">
+            Decks
+          </h1>
+        </div>
       </b-navbar-brand>
     </b-navbar>
 </template>
