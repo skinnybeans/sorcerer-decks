d8a35ca5603d5fc1b8ebb5e65cda588e3dbf6395
Henrik <skinnybeans@gmail.com>
2019-09-10 12:11:28 +1000

    removed unneeded code from packselection

--- src/components/packs/PackSelection.vue
+++ src/components/packs/PackSelection.vue
@@ -26,40 +26,6 @@ import appPackList from './PackList.vue';
 
 export default {
   computed: {
-    // selectedCharacters: {
-    //   get() {
-    //     return this.$store.state.packs.selectedCharacters;
-    //   },
-    //   set(value) {
-    //     this.$store.commit('packs/setSelectedCharacters', value);
-    //   },
-    // },
-    selectedLinages: {
-      get() {
-        const selected = this.$store.getters['packs/selectedPacksByType']('linage');
-        return selected;
-        // return this.$store.state.packs.selectedLinages;
-      },
-      set(value) {
-        // this.$store.commit('packs/setSelectedLinages', value);
-        const selected = this.$store.getters['packs/selectedPacksByType']('linage');
-        if (JSON.stringify(selected) !== JSON.stringify(value)) {
-          const data = { type: 'linage', packs: value };
-          this.$store.dispatch('packs/setSelectedByType', data);
-        }
-      },
-    },
-    selectedDomains: {
-      get() {
-        return this.$store.state.packs.selectedDomains;
-      },
-      set(value) {
-        this.$store.commit('packs/setSelectedDomains', value);
-      },
-    },
-    characters() { return this.$store.state.packs.characters; },
-    linages() { return this.$store.state.packs.linages; },
-    domains() { return this.$store.state.packs.domains; },
   },
   data() {
     return {};
