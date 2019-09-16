1d0cb08d0775a3b7a8ff9280f658cf1219512a6c
Henrik <skinnybeans@gmail.com>
2019-09-16 12:00:10 +1000

    added vue router and start of card list

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import store from './store/store';
+
+import { routes } from './routes';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
 Vue.use(BootstrapVue);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
 
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app');
