d6db56e4299e9ded6d0a5659300f61054ebb4938
Henrik <skinnybeans@gmail.com>
2019-09-17 16:00:03 +1000

    added nav options for cards and decks

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import appCardList from './components/card-list/CardList.vue';
 
 export const routes = [
   { path: '/cardlist', component: appCardList },
-  { path: '/generatedeck', component: appDeckGeneration },
+  { path: '/deckgeneration', component: appDeckGeneration },
   { path: '/', component: appDeckGeneration },
 ];
 export { routes as default };
