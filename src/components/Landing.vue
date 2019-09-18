59b4270dcc73f9c5f9e90b6468b79acd0473988c
Henrik <skinnybeans@gmail.com>
2019-09-18 10:52:10 +1000

    added a landing page

---
+++ src/components/Landing.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <section>
+      <h1>Welcome to Sorcerer Decks</h1>
+      <p>This site has been created to randomly generate decks for Sorcerer by White Wizard Games.</p>
+      <p>The current card list can also be accessed here.</p>
+    </section>
+    <section>
+      <p>
+        Check out the
+          <b-link
+            to="/deckgeneration"
+            class=""
+            >
+            Deck Generator
+          </b-link>
+      </p>
+      <p>
+        Check out the
+        <b-link
+          to="/cardlist"
+          >
+          Card List
+        </b-link>
+      </p>
+    </section>
+    <section>
+      <p>If you want to know more about Sorcerer visit the offical page at: <a href="https://www.sorcerergame.com/" target="_blank">www.sorcerergame.com</a></p>
+    </section>
+  </div>
+</template>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import appDeckGeneration from './components/deck-generation/DeckGeneration.vue';
 import appCardList from './components/card-list/CardList.vue';
+import appLanding from './components/Landing.vue';
 
 export const routes = [
   { path: '/cardlist', component: appCardList },
   { path: '/deckgeneration', component: appDeckGeneration },
-  { path: '/', component: appDeckGeneration },
+  { path: '/', component: appLanding },
 ];
 export { routes as default };
